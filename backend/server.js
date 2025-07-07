4a480c08ab5da4c8524ebce0dbf4bc48e3a47952
ShivaMahaur1 <shivamahaur714@gmail.com>
2025-07-07 23:48:56 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -28,5 +28,4 @@ app.post("/api/gemini", async (req, res) => {
   }
 });
 
-const PORT = 5000
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(process.env.PORT || 5000)
